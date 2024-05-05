14ae43d3df026424915176a235790911cd4071b0
diksha <rajdiksha91@gmail.com>
2024-05-06 04:17:08 +0530

    initial set up

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Intro from "./components/Intro";
+import Instructions from "./components/Instructions";
+import Create from "./components/Create";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Intro />
+      <Instructions />
+      <Create />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
